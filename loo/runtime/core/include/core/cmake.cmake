7b97c7a78026ecc0c657a1b5ee8699f558202e54
noleafnolife <770829892@qq.com>
2020-02-27 13:01:59 +0800

    1

--- loo/runtime/core/include/core/cmake.cmake
+++ loo/runtime/core/include/core/cmake.cmake
@@ -1,17 +1,22 @@
 
-set(LOCAL_PUBLIC     
-${MODULE_ROOT_PATH}/include/core/context.h
-${MODULE_ROOT_PATH}/include/core/dllexporter.h
-)
+# set(LOCAL_PUBLIC     
+# ${MODULE_ROOT_PATH}/include/core/context.h
+# ${MODULE_ROOT_PATH}/include/core/dllexporter.h
+# )
 
-set(LOCAL_PRIVATE
-   ${MODULE_ROOT_PATH}/source/core/context.cpp
-   ${MODULE_ROOT_PATH}/source/core/declreflect.h
-)
+# set(LOCAL_PRIVATE
+   # ${MODULE_ROOT_PATH}/source/core/context.cpp
+   # ${MODULE_ROOT_PATH}/source/core/declreflect.h
+# )
+
+file( GLOB_RECURSE LOCAL_PUBLIC "*.h" )
+file( GLOB_RECURSE LOCAL_PRIVATE "*.cpp" )
+source_group( TREE ${CMAKE_CURRENT_SOURCE_DIR} FILES ${LOCAL_PUBLIC} )
+source_group( TREE ${CMAKE_CURRENT_SOURCE_DIR} FILES ${LOCAL_PRIVATE} )
 
 
-source_group("core\\include\\core" FILES ${LOCAL_PUBLIC})
-source_group("core\\source\\core" FILES ${LOCAL_PRIVATE})
+#source_group("core\\include\\core" FILES ${LOCAL_PUBLIC})
+#source_group("core\\source\\core" FILES ${LOCAL_PRIVATE})
 
 
 set(SOURCE_PUBLIC ${SOURCE_PUBLIC} ${LOCAL_PUBLIC})
---
+++ loo/runtime/core/include/core/object/generate/object.loo.h
@@ -0,0 +1,81 @@
+/* this file is auto-generated. do not edit! */
+#pragma once
+#include "looreflect/looreflect.h"
+#ifndef __LOOREFLECT__
+#include "looreflect/looreflectmanager.h"
+#endif
+namespace looreflect
+{
+
+template<> struct IsSerializable<loo::Object> { static constexpr bool value = true; };
+template<> struct HasBeforeSerialize<loo::Object> { static constexpr bool value = false; };
+template<> struct HasAfterSerialize<loo::Object> { static constexpr bool value = false; };
+template<> struct HasCustomSerialize<loo::Object> { static constexpr bool value = false; };
+template<> struct HasCustomDump<loo::Object> { static constexpr bool value = false; };
+namespace detail
+{
+template<>
+LooClass const *
+LooGetClassImpl(LooClassTag<loo::Object>) noexcept
+{
+static detail::LooClassBuilder<loo::Object, 1, 2> reflected([](auto self) {
+
+/* LooField 1 */
+self->fields[0].m_type = LooGetType<char>();
+self->fields[0].m_flags = LooField::kFlagsNull | LooField::kFlagsSerialized | LooField::kFlagsCString;
+self->fields[0].m_serializedWidth = sizeof(char) * 8;
+self->fields[0].m_offset = offsetof(loo::Object, m_Name);
+self->fields[0].m_qualifier = Qualifier(0, 0, 0, 1, 0, 0);
+self->fields[0].m_name = "m_Name";
+
+/* LooFunction 1*/
+static LooFunctionReturn functionRet0;
+functionRet0.m_type = LooGetType<char>();
+functionRet0.m_flags = 0/*TODO*/;
+functionRet0.m_serializedWidth = sizeof(char) * 8;
+functionRet0.m_qualifier = Qualifier(0, 0, 0, 1, 0, 0);
+static LooFunctionParameter functionParameters0[1];
+self->functions[0].m_returnType = &functionRet0;
+self->functions[0].m_parameters = &functionParameters0[0];
+self->functions[0].m_parametersEnd = &functionParameters0[0];
+self->functions[0].m_flags = LooFunction::kFlagsNull | LooFunction::kFlagsMember;
+self->functions[0].m_name = "name";
+
+/* LooFunction 2*/
+static LooFunctionReturn functionRet1;
+functionRet1.m_type = LooGetType<char>();
+functionRet1.m_flags = 0/*TODO*/;
+functionRet1.m_serializedWidth = sizeof(char) * 8;
+functionRet1.m_qualifier = Qualifier(0, 0, 0, 1, 0, 0);
+static LooFunctionParameter functionParameters1[1];
+self->functions[1].m_returnType = &functionRet1;
+self->functions[1].m_parameters = &functionParameters1[0];
+self->functions[1].m_parametersEnd = &functionParameters1[0];
+self->functions[1].m_flags = LooFunction::kFlagsNull | LooFunction::kFlagsMember;
+self->functions[1].m_name = "name";
+});
+static LooClass cache(
+sizeof(loo::Object),
+Hash("loo::Object"),
+nullptr,
+reflected.fields,
+reflected.fields + reflected.numFields,
+reflected.functions,
+reflected.functions + reflected.numFunctions,
+"loo::Object",
+LooClass::kFlagsNull);
+return &cache;
+}
+
+template<>
+LooType const *
+LooGetTypeImpl(LooTypeTag<loo::Object>) noexcept
+{
+return LooGetClass<loo::Object>();
+}
+} /* namespace detail */
+} /* namespace looreflect */
+
+// utils functions for class instance
+const looreflect::LooType *loo::Object::GetType() const { return looreflect::LooGetClass<loo::Object>();}
+
